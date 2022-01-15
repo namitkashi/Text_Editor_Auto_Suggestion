5c99e257d605f6597b15e013b6d86c2af3d9e989
namitkashi <91289904+namitkashi@users.noreply.github.com>
2022-01-15 15:55:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -109,16 +109,13 @@ function App() {
 	}
 
 	function coords() {
-		// The properties that we copy into a mirrored div.
-		// Note that some browsers, such as Firefox,
-		// do not concatenate properties, i.e. padding-top, bottom etc. -> padding,
-		// so we have to do every single property specifically.
+		
 		var properties = [
 			'boxSizing',
-			'width',  // on Chrome and IE, exclude the scrollbar, so the mirror div wraps exactly as the textarea does
+			'width',  
 			'height',
 			'overflowX',
-			'overflowY',  // copy the scrollbar for IE
+			'overflowY',  
 
 			'borderTopWidth',
 			'borderRightWidth',
@@ -130,7 +127,7 @@ function App() {
 			'paddingBottom',
 			'paddingLeft',
 
-			// https://developer.mozilla.org/en-US/docs/Web/CSS/font
+			
 			'fontStyle',
 			'fontVariant',
 			'fontWeight',
@@ -142,7 +139,7 @@ function App() {
 			'textAlign',
 			'textTransform',
 			'textIndent',
-			'textDecoration',  // might not make a difference, but better be safe
+			'textDecoration',  
 
 			'letterSpacing',
 			'wordSpacing'
@@ -173,7 +170,7 @@ function App() {
 			style.position = 'absolute';  // required to return coordinates properly
 			style.top = element.offsetTop + parseInt(computed.borderTopWidth) + 'px';
 			style.left = "400px";
-			style.visibility = mirrorDivDisplayCheckbox.checked ? 'visible' : 'hidden';  // not 'display: none' because we want rendering
+			style.visibility = mirrorDivDisplayCheckbox.checked ? 'visible' : 'hidden';  
 
 			// transfer the element's properties to the div
 			properties.forEach(function (prop) {
@@ -181,8 +178,8 @@ function App() {
 			});
 
 			if (isFirefox) {
-				style.width = parseInt(computed.width) - 2 + 'px'  // Firefox adds 2 pixels to the padding - https://bugzilla.mozilla.org/show_bug.cgi?id=753662
-				// Firefox lies about the overflow property for textareas: https://bugzilla.mozilla.org/show_bug.cgi?id=984275
+				style.width = parseInt(computed.width) - 2 + 'px'  
+				
 				if (element.scrollHeight > parseInt(computed.height))
 					style.overflowY = 'hidden';
 			} else {
@@ -190,17 +187,13 @@ function App() {
 			}
 
 			mirrorDiv.textContent = element.value.substring(0, position);
-			// the second special handling for input type="text" vs textarea: spaces need to be replaced with non-breaking spaces - http://stackoverflow.com/a/13402035/1269037
+			
 			if (element.nodeName === 'INPUT')
 				mirrorDiv.textContent = mirrorDiv.textContent.replace(/\s/g, "\u00a0");
 
 			var span = document.createElement('span');
-			// Wrapping must be replicated *exactly*, including when a long word gets
-			// onto the next line, with whitespace at the end of the line before (#7).
-			// The  *only* reliable way to do that is to copy the *entire* rest of the
-			// textarea's content into the <span> created at the caret position.
-			// for inputs, just '.' would be enough, but why bother?
-			span.textContent = element.value.substring(position) || '.';  // || because a completely empty faux span doesn't render at all
+			
+			span.textContent = element.value.substring(position) || '.';  
 			span.style.backgroundColor = "lightgrey";
 			mirrorDiv.appendChild(span);
 
